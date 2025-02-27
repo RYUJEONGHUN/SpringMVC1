780ad02ad132434d2befdb9814316673f7c8e4eb
RYUJEONGHUN <ryugh0210@naver.com>
2025-02-28 01:00:02 +0900

    first servlet

---
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,17 @@
+package hello.servlet.basic;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+import java.io.IOException;
+
+@WebServlet(name="helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        System.out.println("HelloServlet.service");
+    }
+}
