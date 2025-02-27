66479c6f4b2b61f4a47635d2d5d52a28c2880c28
RYUJEONGHUN <ryugh0210@naver.com>
2025-02-28 01:31:36 +0900

    쿼리 파라미터 출력

--- src/main/java/hello/servlet/basic/HelloServlet.java
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -11,7 +11,9 @@ import java.io.IOException;
 @WebServlet(name="helloServlet", urlPatterns = "/hello")
 public class HelloServlet extends HttpServlet {
     @Override
-    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
-        System.out.println("HelloServlet.service");
+    public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+
+        String username= request.getParameter("username");
+        System.out.println("username="+username);
     }
 }
