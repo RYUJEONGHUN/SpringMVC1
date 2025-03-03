bfc7c65902cd1ccbc2b5ae357b77dab86efec806
RYUJEONGHUN <ryugh0210@naver.com>
2025-03-03 18:38:29 +0900

    GET 쿼리 파라미터
    HTTP 요청 데이터 - GET 쿼리 파라미터

--- src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
@@ -88,6 +88,7 @@ public class RequestHeaderServlet extends HttpServlet {
         System.out.println();
     }
 
+
     private void printEtc(HttpServletRequest request) {
         System.out.println("--- 기타 조회 start ---");
         System.out.println("[Remote 정보]");
